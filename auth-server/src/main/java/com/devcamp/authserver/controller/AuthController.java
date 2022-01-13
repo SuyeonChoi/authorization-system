f04fc94de5ec0ba7039196ccd66ae6aec6b4a76d
SuyeonChoi <tndus970120@gmail.com>
2022-01-13 13:06:35 +0900

    fix: 오타 수정 및 CrossOrigin 해결

--- auth-server/src/main/java/com/devcamp/authserver/controller/AuthController.java
+++ auth-server/src/main/java/com/devcamp/authserver/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.devcamp.authserver.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import javax.validation.Valid;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
--- user-server/src/main/java/com/devcamp/userserver/controller/UserController.java
+++ user-server/src/main/java/com/devcamp/userserver/controller/UserController.java
@@ -5,6 +5,7 @@ import com.devcamp.userserver.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @Slf4j
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
